04261d0da8ef573023c844f32eb105c633389aa5
Michel Schaub <michel.schaub@semita-gmbh.ch>
2026-06-12 15:58:33 +0200

    Temporary admin setup

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import logout
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.models import User
 
+from django.http import HttpResponse
 from django.shortcuts import redirect
 
 
@@ -16,3 +18,22 @@ def logout_view(request):
     logout(request)
 
     return redirect("/login/")
+
+
+def setup_admin(request):
+
+    if User.objects.filter(username="admin").exists():
+
+        return HttpResponse(
+            "Admin existiert bereits."
+        )
+
+    User.objects.create_superuser(
+        username="admin",
+        email="michel.schaub@semita-gmbh.ch",
+        password="Admin2026!"
+    )
+
+    return HttpResponse(
+        "Admin erstellt."
+    )
--- config/urls.py
+++ config/urls.py
@@ -5,15 +5,15 @@ from django.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from accounts.views import SemitaLoginView
+from accounts.views import setup_admin
 
 
 urlpatterns = [
 
     path(
-        "login/",
-        SemitaLoginView.as_view(),
-        name="login"
+    "setup-admin/",
+    setup_admin,
+    name="setup_admin"
     ),
 
     path(
